d8fa8d8f5cae055225b0fe793d36a1f6b64762b1
Prashant Singh <prashantsh7014@gmail.com>
2024-09-17 04:33:30 +0530

    add solution

--- frontend/src/components/Ministryofcoal.jsx
+++ frontend/src/components/Ministryofcoal.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MinistryOfCoal = () => {
   return (
-    <div className="flex flex-col md:flex-row bg-gray-50 p-6">
+    <div className="flex">
     
     </div>
   );
